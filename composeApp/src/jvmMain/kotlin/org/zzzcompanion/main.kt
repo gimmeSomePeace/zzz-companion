c111d8c24b493c6c17c64d5df22c87b7cc189bfe
gimmeSomePeace <toadmasterhole@gmail.com>
2026-04-06 16:36:15 +0500

    initial commit

---
+++ composeApp/src/jvmMain/kotlin/org/zzzcompanion/main.kt
@@ -0,0 +1,10 @@
+package org.zzzcompanion
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication, title = "ZZZ Companion") {
+    }
+}
