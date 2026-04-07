a63cc414d28565d274b92c1159aa23978cfe506a
gimmeSomePeace <toadmasterhole@gmail.com>
2026-04-07 14:53:57 +0500

    feat: изменены начальные параметры стартового окна и установлена иконка приложения

--- composeApp/src/jvmMain/kotlin/org/zzzcompanion/main.kt
+++ composeApp/src/jvmMain/kotlin/org/zzzcompanion/main.kt
@@ -1,10 +1,27 @@
 package org.zzzcompanion
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import org.jetbrains.compose.resources.painterResource
+import zzz_companion.composeapp.generated.resources.Res
+import zzz_companion.composeapp.generated.resources.icon
 
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "ZZZ Companion") {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ZZZ Companion",
+        icon = painterResource(Res.drawable.icon),
+        state = rememberWindowState(
+            width = 800.dp,
+            height = 600.dp,
+            position = WindowPosition.Aligned(Alignment.Center),
+        )
+    ) {
+        App()
     }
 }
