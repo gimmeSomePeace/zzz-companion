9b5575a00862384fe400903657bd57cf0f7d82e9
gimmeSomePeace <toadmasterhole@gmail.com>
2026-04-24 20:36:27 +0500

    fix: исправить баг при добавлении персонажа в список имеющихся

--- composeApp/src/commonMain/kotlin/org/gimmesomepeace/zzzcompanion/data/storage/local/characteruserdata/CharacterUserDataLocalMapper.kt
+++ composeApp/src/commonMain/kotlin/org/gimmesomepeace/zzzcompanion/data/storage/local/characteruserdata/CharacterUserDataLocalMapper.kt
@@ -16,11 +16,11 @@ fun List<CharacterUserDataLocalEntity>.toDomain(): List<CharacterUserData> = map
 fun CharacterUserData.toLocalEntity() : CharacterUserDataLocalEntity = CharacterUserDataLocalEntity(
     id = id.value,
     characterId = characterId.value,
-    disks1Id = disks[0].id,
-    disks2Id = disks[1].id,
-    disks3Id = disks[2].id,
-    disks4Id = disks[3].id,
-    disks5Id = disks[4].id,
-    disks6Id = disks[5].id,
+    disks1Id = disks.getOrNull(0)?.id,
+    disks2Id = disks.getOrNull(1)?.id,
+    disks3Id = disks.getOrNull(2)?.id,
+    disks4Id = disks.getOrNull(3)?.id,
+    disks5Id = disks.getOrNull(4)?.id,
+    disks6Id = disks.getOrNull(5)?.id,
 
 )
